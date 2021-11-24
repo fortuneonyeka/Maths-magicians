cba578bad4791233611c48561fa79b52a1d3fe28
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-24 16:04:29 +0100

    Add onClick function to the buttons

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -18,25 +18,25 @@ class Calculator extends React.Component {
           {next}
         </div>
         <div className="keypads">
-          <button id="reset" name="AC">AC</button>
-          <button id="backspace" name="+/-">+/-</button>
-          <button name="%">%</button>
-          <button className="operator" name="÷">÷</button>
-          <button name="7">7</button>
-          <button name="8">8</button>
-          <button name="9">9</button>
-          <button className="operator" name="x">x</button>
-          <button name="4">4</button>
-          <button name="5">5</button>
-          <button name="6">6</button>
-          <button className="operator" name="-">-</button>
-          <button name="1">1</button>
-          <button name="2">2</button>
-          <button name="3">3</button>
-          <button className="operator" id="plus" name="+">+</button>
-          <button name="0" className="zero">0</button>
-          <button name=".">.</button>
-          <button className="operator grid">=</button>
+          <button id="reset" name="AC" onClick={handleClick}>AC</button>
+          <button id="backspace" name="+/-" onClick={handleClick}>+/-</button>
+          <button name="%" onClick={handleClick}>%</button>
+          <button className="operator" name="÷" onClick={handleClick}>÷</button>
+          <button name="7" onClick={handleClick}>7</button>
+          <button name="8" onClick={handleClick}>8</button>
+          <button name="9" onClick={handleClick}>9</button>
+          <button className="operator" name="x" onClick={handleClick}>x</button>
+          <button name="4" onClick={handleClick}>4</button>
+          <button name="5" onClick={handleClick}>5</button>
+          <button name="6" onClick={handleClick}>6</button>
+          <button className="operator" name="-" onClick={handleClick}>-</button>
+          <button name="1" onClick={handleClick}>1</button>
+          <button name="2" onClick={handleClick}>2</button>
+          <button name="3" onClick={handleClick}>3</button>
+          <button className="operator" id="plus" name="+" onClick={handleClick}>+</button>
+          <button name="0" className="zero" onClick={handleClick}>0</button>
+          <button name="." onClick={handleClick}>.</button>
+          <button className="operator grid" onClick={handleClick}>=</button>
         </div>
       </div>
 
