5b1590763aff0de7dd41b4ce11e307d552bb2a25
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-24 11:58:00 +0100

    add math operation logics

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,28 @@
 
+import Big from 'big.js';
+
+export default function operate(numberOne, numberTwo, operation) {
+  const one = Big(numberOne);
+  const two = Big(numberTwo);
+  if (operation === '+') {
+    return one.plus(two).toString();
+  }
+
+  if (operation === '-') {
+    return one.minus(two).toString();
+  }
+  if (operation === '*') {
+    return one.times(two).toString();
+  }
+  if (operation === '÷') {
+    try {
+      return one.div(two).toString();
+    } catch (err) {
+      return 'Not dividable by 0.';
+    }
+  }
+  if (operation === '%') {
+    return one.mod(two).toString;
+  }
+  throw Error(`Unknown oeration '${operation}'`);
+}
