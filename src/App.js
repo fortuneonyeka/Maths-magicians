de0246bb83a7a571adb11f28b4b6c59575668fdb
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-25 17:29:17 +0100

    Refactor app from class comonent to function component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-/* eslint-disable  */
+import { useState } from 'react';
 import './App.css';
-import { Component } from 'react';
 import Calculator from './component/Calculator';
 import calculate from './logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = ({target}) => {
-    const result = calculate(this.state, target.name);
-    this.setState(result)
-  }
-
-  render() {
-    const {total, next, operation} = this.state;
-    return (
-      <Calculator handleClick={this.handleClick} total={total} operation={operation} next={next}/>
-    );
-  }
-}
+const App = () => {
+  const [obj, setObj] = useState({});
+  const handleClick = ({ target }) => {
+    const result = calculate(obj, target.name);
+    setObj(result);
+  };
+  return (
+    <Calculator handleClick={handleClick} obj={obj} />
+  );
+};
 
 export default App;
