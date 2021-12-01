4da1333cb810fe4f261bd28b026f55bcc96673a3
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-01 10:35:58 +0100

    add quote and home pages

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+/* eslint-disable  */
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div className="error-msg">
+      <p>Error, Page not found</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
---
+++ src/pages/Quotes.js
@@ -0,0 +1,19 @@
+/* eslint-disable  */
+import React from 'react';
+
+function Quote() {
+  return (
+    
+<div>
+    <section className="home1">
+      <div className="content1">
+        <p>
+          Mathematics is not about numbers, equations, computations or
+          algorithms. It is about understanding - William Paul Thurston
+        </p>
+      </div>
+    </section>
+  </div>
+  )
+};
+export default Quote;
