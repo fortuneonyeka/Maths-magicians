c2ed75a037230234aa1ff2b376e7215d42961451
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-01 10:36:38 +0100

    add route logics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import { useState } from 'react';
-import './App.css';
-import Calculator from './component/Calculator';
-import calculate from './logic/calculate';
+/* eslint-disable  */
+import { Component } from "react";
+import "./App.css";
+import Calculator from "./component/Calculator";
+import calculate from "./logic/calculate";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Quote from "./pages/Quotes";
+import NavBar from "./component/Navbar";
+import ErrorPage from "./pages/ErrorPage";
 
-const App = () => {
-  const [obj, setObj] = useState({});
-  const handleClick = ({ target }) => {
-    const result = calculate(obj, target.name);
-    setObj(result);
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+ handleClick = ({ target }) => {
+    const result = calculate(this.state, target.name);
+    this.setState(result);
+  }
+   render() {
+     const { total, next, operation } = this.state;
   return (
-    <Calculator handleClick={handleClick} obj={obj} />
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route
+            path="/calculator"
+            element={
+              <Calculator
+                handleClick={this.handleClick}
+                total={total}
+                operation={operation}
+                next={next}
+              />
+            }
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </>
   );
+   }
 };
 
 export default App;
