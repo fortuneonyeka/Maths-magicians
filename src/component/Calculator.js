9dccd6638134f909190b1c441cdb023641451c48
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-01 16:50:40 +0100

    update calculator styling

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -33,13 +33,13 @@ const Calculator = (props) => {
           >
             ÷
           </button>
-          <button type="button" name="7" onClick={handleClick}>
+          <button className="btn" type="button" name="7" onClick={handleClick}>
             7
           </button>
-          <button type="button" name="8" onClick={handleClick}>
+          <button className="btn" type="button" name="8" onClick={handleClick}>
             8
           </button>
-          <button type="button" name="9" onClick={handleClick}>
+          <button className="btn" type="button" name="9" onClick={handleClick}>
             9
           </button>
           <button
@@ -50,13 +50,13 @@ const Calculator = (props) => {
           >
             x
           </button>
-          <button type="button" name="4" onClick={handleClick}>
+          <button className="btn" type="button" name="4" onClick={handleClick}>
             4
           </button>
-          <button type="button" name="5" onClick={handleClick}>
+          <button className="btn" type="button" name="5" onClick={handleClick}>
             5
           </button>
-          <button type="button" name="6" onClick={handleClick}>
+          <button className="btn" type="button" name="6" onClick={handleClick}>
             6
           </button>
           <button
@@ -67,13 +67,13 @@ const Calculator = (props) => {
           >
             -
           </button>
-          <button type="button" name="1" onClick={handleClick}>
+          <button className="btn" type="button" name="1" onClick={handleClick}>
             1
           </button>
-          <button type="button" name="2" onClick={handleClick}>
+          <button className="btn" type="button" name="2" onClick={handleClick}>
             2
           </button>
-          <button type="button" name="3" onClick={handleClick}>
+          <button className="btn" type="button" name="3" onClick={handleClick}>
             3
           </button>
           <button
